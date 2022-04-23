9f89378ffcd05a081e592c79228aa34bc16e1b48
keon4now <keon4now@gmail.com>
2022-04-23 11:33:39 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />); // creates main interface that we will work on...main hook...tells react where UI should be placed
+
+//index.html is located in public with root div...react is a single page so everything will be rendered there
