b791899e9c3d60ea122cf53d5ce0e79914e1e720
keon4now <keon4now@gmail.com>
2022-04-29 18:53:51 -0400

    updates

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -2,12 +2,6 @@ import "./ExpenseDate.css";
 
 function ExpenseDate(props) {
 
-	console.log("testing");
-	console.log("testing");
-	console.log("testing");
-	console.log("testing");
-	console.log("testing");
-
 	const month = props.date.toLocaleString("en-us", { month: "long" });
 	const day = props.date.toLocaleString("en-us", { day: "2-digit" });
 	const year = props.date.getFullYear();
